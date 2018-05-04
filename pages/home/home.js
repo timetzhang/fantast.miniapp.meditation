fd33b3d613dced0b54a31681ed31482a8d0886c2
hjcshaqiu@163.com <hjcshaqiu@163.com>
2018-05-04 16:47:37 +0800

    timer completion

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,15 @@
 // pages/home/home.js
 let globalBgAudioManager = getApp().globalData.backgroundAudioManager;
 var currentMusicInfo = getApp().globalData.currentMusicInfo;
+var timer;
+const hours=[];
+const minutes = [];
+for(let i = 0; i <= 12; i++){
+  hours.push(i)
+}
+for (let i = 1; i <= 60; i++) {
+  minutes.push(i)
+}
 Page({
   /**
    * 页面的初始数据
@@ -15,6 +24,16 @@ Page({
     playStatusImage:'../../images/play.png',
     repeatStatus:true,
     repeatStatusImage:'../../images/repeat.png',
+    timerValue: 0,
+    selectTimeArray:[],
+    selectTimeIndex:0,
+    showModalStatus:false,
+    //timeSelect
+    hours:hours,
+    hour:0,
+    minutes:minutes,
+    minute:1,
+    timeValue: [0,1],
   },
 
   /**
@@ -94,6 +113,135 @@ Page({
     this.playTargetMusic(status);
   }, 
   /**
+   * 定时器选择器
+   */
+  bindClockChange:function(){
+    var that = this;
+    this.clearTimer();
+    wx.showActionSheet({
+      itemList: ['关闭', '10分钟后', '20分钟后', '30分钟后', '60分钟后', '自定义'],
+      success: function(res){
+        var index = res.tapIndex;
+        var timeValue = 0;
+        if (index == 0) {
+          wx.showToast({
+            title: '已关闭定时',
+            icon: 'success',
+            duration: 2000
+          })
+          return;
+        }
+        if (index == 5) {
+          that.util('open')
+        } else {
+          if (index == 4) {
+            timeValue = 6*600000;
+          }else{
+            timeValue = index * 600000;
+          }
+          that.timerStart(timeValue);
+        }
+      },
+      fail:function(err){
+
+      }
+    })
+  },
+  /**
+   * 自定义定时
+   */
+  bindChange: function (e) {
+    const val = e.detail.value
+    this.setData({
+      hour: this.data.hours[val[0]],
+      minute: this.data.minutes[val[1]],
+    })
+  },
+  /**
+   * 确认是否开启定时
+   */
+  setClock:function(e){
+    var currentStatu = e.currentTarget.dataset.statu;
+    if(currentStatu == 'set'){
+      var timeValue = this.data.hour * 60 + this.data.minute;
+      this.timerStart(timeValue*60000);
+    } else if (currentStatu == 'cancel'){
+      this.setData({
+        hour:0,
+        minute:1
+      })
+    }
+    this.util('close');
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例   
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长  
+      timingFunction: "linear", //线性  
+      delay: 0  //0则不延迟  
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例  
+    this.animation = animation;
+
+    // 第3步：执行第一组动画  
+    animation.opacity(0).rotateX(-100).step();
+
+    // 第4步：导出动画对象赋给数据对象储存  
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画  
+    setTimeout(function () {
+      // 执行第二组动画  
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭  
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示  
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+  },
+  /**
+   * 开启定时
+   */
+  timerStart: function(timeValue){
+    var that = this;
+    var showValue = timeValue;
+    timer = setTimeout(function () {
+      if (globalBgAudioManager.paused != 'undefined' && globalBgAudioManager.paused != true){
+        that.playTargetMusic(false);
+      }
+    }, timeValue)
+  },
+  /**
+   * 停止定时器
+   */
+  clearTimer:function(){
+    if(timer){
+      clearTimeout(timer);
+    }
+  },
+  /**
    * 播放目标音乐
    */
   playTargetMusic:function(status){
@@ -112,6 +260,7 @@ Page({
     }else{
       playStatusImage = '../../images/play.png';
       globalBgAudioManager.pause()
+      this.clearTimer();
     }
     this.setData({
       paused: !status,
@@ -156,6 +305,7 @@ Page({
   onShow: function () {
     if (globalBgAudioManager.paused) {
       globalBgAudioManager.pause();
+      this.clearTimer();
       this.setData({
         paused: globalBgAudioManager,
         playStatusImage : '../../images/play.png'
